b8e94931cb8604d7af8d74729d6ad81b1950b1ef
Francismario <francismario.silva@softplan.com.br>
2024-04-20 16:41:45 -0300

    remove test

--- src/test/java/CalculatorTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class CalculatorTests {
-    @Test
-    public void testAdd() {
-        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
-    }
-}
